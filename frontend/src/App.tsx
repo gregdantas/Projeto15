67df02e805608cb77b22ba4a91e0b85b77e4c570
gregdantas <gregorydantasjc@hotmail.com>
2022-07-16 02:26:50 -0300

    add componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
   return (
-  <>
-<h1>Iniciando Projeto 15/07</h1>
-   <NotificationButton/>
-   </>
+    <>
+
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <SalesCard/>
+          </div>
+        </section>
+      </main>
+
+
+    </>
   )
 }
 
