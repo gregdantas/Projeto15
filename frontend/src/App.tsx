fa2cc669910dfb4699ee8f3e64460e1aad9ac7fd
gregdantas <gregorydantasjc@hotmail.com>
2022-07-16 01:19:16 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return (
+  <>
 <h1>Iniciando Projeto 15/07</h1>
-
+   <NotificationButton/>
+   </>
   )
-
 }
 
 export default App
