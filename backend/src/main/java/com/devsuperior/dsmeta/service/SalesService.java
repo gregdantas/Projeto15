719ddbf4bd6ca8862891434fe2f975f3568b1df7
gregdantas <gregorydantasjc@hotmail.com>
2022-07-20 01:23:05 -0300

    Select por data

--- backend/src/main/java/com/devsuperior/dsmeta/service/SalesService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SalesService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -11,9 +15,31 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 public class SalesService {
 	
 	@Autowired
-	private SaleRepository repository ; 
-	public List<Sale> findSales() {
-		return repository.findAll() ; 
+	private SaleRepository repository ; /*instancia auto..*/
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()) ; /*metodo para capturar hora atual do sistema*/
+		LocalDate max ; 
+		LocalDate min ; 
+//		LocalDate min =LocalDate.parse(minDate);
+//		LocalDate max =LocalDate.parse(maxDate);
+        if(maxDate.equals("")) {
+        	max = today ; 
+        }else {
+        	max = LocalDate.parse(maxDate); 
+        }
+        
+        if(minDate.equals("")) {
+        	min = today.minusYears(1) ; 
+        }else {
+        	min = LocalDate.parse(minDate);
+        }
+        
+		
+		return repository.findSales(min, max, pageable) ; 
 	}
  
+	
+	
 }
+ 
